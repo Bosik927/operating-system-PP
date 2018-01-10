6541a02f05c44ff605377eda96eb66e56bf035ee
yawen0 <34008441+yawen0@users.noreply.github.com>
2018-01-10 22:22:04 +0100

    Update Komunikacja.cpp

--- Komunikacja.cpp
+++ Komunikacja.cpp
@@ -10,24 +10,26 @@ Komunikacja::Komunikacja(ProcessManagement *Proces)
 void Komunikacja::write(string id, string msg)
 {
 	lock->lock(*(Proces)->getPCB(Proces->getIdFromName(id)));
-	mkfifo.zapisz(msg);
+	mkfif.zapisz(msg);
 	lock->unlock(*(Proces)->getPCB(Proces->getIdFromName(id)));
 }
 
-void Komunikacja::read(string id)
-{
-	lock->lock(*(Proces)->getPCB(Proces->getIdFromName(id)));
-	mkfifo.odczytaj();
-	mkfifo.fifo.open(mkfifo.sciezka, ios::trunc);
-	mkfifo.fifo.close();
-	lock->unlock(*(Proces)->getPCB(Proces->getIdFromName(id)));
-}
+//void Komunikacja::read(string id)
+//{
+//	lock->lock(*(Proces)->getPCB(Proces->getIdFromName(id)));
+//	mkfif.odczytaj();
+//	mkfif.fifo.open(mkfif.sciezka, ios::trunc);
+//	mkfif.fifo.close();
+//	lock->unlock(*(Proces)->getPCB(Proces->getIdFromName(id)));
+//}
 
 void Komunikacja::read(string id, int iloscz)
 {
 	lock->lock(*(Proces)->getPCB(Proces->getIdFromName(id)));
-	mkfifo.odczytaj(iloscz);
-	mkfifo.fifo.open(mkfifo.sciezka, ios::trunc);
-	mkfifo.fifo.close();
+	mkfif.odczytaj(iloscz);
+	mkfif.fifo.open(mkfif.sciezka, ios::trunc);
+	mkfif.fifo.close();
 	lock->unlock(*(Proces)->getPCB(Proces->getIdFromName(id)));
 }
+
+
