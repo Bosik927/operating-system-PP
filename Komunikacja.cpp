22c8304ce647b1a6d30a27616e122e89d6eac01a
yawen0 <34008441+yawen0@users.noreply.github.com>
2018-01-13 12:33:17 +0100

    Update Komunikacja.cpp

--- Komunikacja.cpp
+++ Komunikacja.cpp
@@ -25,11 +25,11 @@ void Komunikacja::write(string id, string msg)
 
 void Komunikacja::read(string id, int iloscz)
 {
-	lock->lock(*(Proces)->getPCB(Proces->getIdFromName(id)));
+	if(lock->lock(*(Proces)->getPCB(Proces->getIdFromName(id)))){
 	mkfif.odczytaj(iloscz);
 	mkfif.fifo.open(mkfif.sciezka, ios::trunc);
 	mkfif.fifo.close();
-	lock->unlock(*(Proces)->getPCB(Proces->getIdFromName(id)));
+	}lock->unlock(*(Proces)->getPCB(Proces->getIdFromName(id)));
 }
 
 
