16f20fba758b0d1bc3dfb66c1e41b5916572c14d
AbuBabi <35044373+AbuBabi@users.noreply.github.com>
2018-01-18 00:51:48 +0100

    O CHUJ CHODZI?

---
+++ MainPCB.cpp
@@ -0,0 +1,57 @@
+//#include"Scheduler.h"
+//#include"ID_Manager.h"
+//#include"ProcessManagement.h"
+//#include"Interpreter.h"
+//#include"plikiFAT.h"
+//#include<iostream>
+//ProcessManagement mng;
+//Interpreter intr(&mng);
+//int main(){
+//
+//	mng.CreateProcess("aaa", "a", 1);
+//	mng.CreateProcess("bbb", "a", 2);
+//	//mng.CreateProcess("ccc", "a");
+//	//mng.CreateProcess("Bambosz ty wuju", "a", 5);
+//	//mng.CreateProcess("ddd", "a", 1);
+//	mng.DisplayScheduler();
+//
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();		
+//		mng.Run();
+//		mng.DisplayScheduler();		
+//		std::cout << std::endl << "sleep" << std::endl;
+//		mng.Sleep(2);
+//		mng.Run();
+//		mng.DisplayScheduler();		
+//		mng.Run();
+//
+//
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();
+//
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		std::cout << std::endl << "wakeUp" << std::endl;
+//		mng.WakeUp(1);
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();
+//		mng.Run();
+//		mng.DisplayScheduler();
+//
+//	return 0;
+//}
