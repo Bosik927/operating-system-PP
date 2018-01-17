a8bbe63532e33703d08514f95d280eaf4cdc6cd1
IvanNovgorodtsev <novgorodtsev@wp.pl>
2018-01-17 21:41:10 +0100

    commit

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -18,7 +18,7 @@ Rozkazy convert(const string& operacja) // Funkcja dla konwersji rozkazow dla sw
 	else if (operacja == "DF") return DF;
 	else if (operacja == "RF") return RF;
 	else if (operacja == "AF") return AF;
-	else if (operacja == "OF") return OF;
+	else if (operacja == "OF") return FC;
 	else if (operacja == "CP") return CP;
 	else if (operacja == "DP") return DP;
 	else if (operacja == "RP") return RP;
@@ -87,11 +87,6 @@ void Interpreter::run(PCB* PCBbox)
 				if (program.substr(3, 1) == "A")
 				{
 					processmanagement->SetReg(PCBbox->ID, 'A', processmanagement->GetReg(PCBbox->ID, 'A')+liczba);
-					std::cout << liczba << "\n";
-					std::cout << PCBbox->ID << "\n";
-					std::cout << processmanagement->GetReg(PCBbox->ID, 'A') << "\n";
-
-
 				}
 				else if (program.substr(3, 1) == "B")
 				{
